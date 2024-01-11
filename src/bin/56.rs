08e1a425b90b9e1b2b0e95a8db2a3ea02db938fb
zscumt123 <zscumt123@163.com>
2024-01-11 17:11:30 +0800

    fix bugs

--- src/bin/56.rs
+++ src/bin/56.rs
@@ -1,5 +1,3 @@
-use std::cmp::Ordering;
-
 fn main() {
     let res = Solution::merge(vec![vec![1, 4], vec![4, 5]]);
 
@@ -8,5 +6,24 @@ fn main() {
 
 pub struct Solution;
 impl Solution {
-    pub fn merge(intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {}
+    pub fn merge(intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        if intervals.len() == 0 {
+            return vec![];
+        }
+        let mut intervals = intervals;
+        intervals.sort();
+
+        let (mut start, mut end) = (intervals[0][0], intervals[0][1]);
+        let mut result = vec![];
+
+        intervals.iter().skip(1).for_each(|x| {
+            if end < x[0] {
+                result.push(vec![start, end]);
+                start = x[0]
+            }
+            end = end.max(x[1]);
+        });
+        result.push(vec![start, end]);
+        result
+    }
 }
